034545306380a73e404e3b14d9a6661e58a8cd81
Mihai Parparita <mihai@persistent.info>
2008-06-21 23:04:01 +0000

    Make the uploader support non-local uploading too.

--- uploader.py
+++ uploader.py
@@ -11,16 +11,19 @@ sys.path.append('/usr/local/google_appengine/lib/yaml/lib')
 from doctype import Doctype
 
 def main(argv):
-  opts, args = getopt.getopt(argv, "", ["doctype=", "group_id="])
+  opts, args = getopt.getopt(argv, "", ["doctype=", "group_id=", "is_local="])
   
   doctype = None
   group_id = None
+  is_local = True
   
   for opt, arg in opts:
     if opt == "--doctype":
       doctype = Doctype.GetDoctypeFromId(arg)
     if opt == "--group_id":
       group_id = arg
+    if opt == "--is_local":
+      is_local = arg == "True"
   
   assert doctype, "Doctype must be given"
   assert group_id, "Group ID must be specified"
@@ -43,12 +46,17 @@ def main(argv):
   csv_file.close()
   
   print "Fetched entries were written to %s" % csv_filename
+  
+  bulkload_path = is_local and \
+      "http://localhost:8080/admin/bulkload" or \
+      "http://multi-doc.appspot.com/admin/bulkload"
+
   print """You should now run:
 /usr/local/google_appengine/tools/bulkload_client.py \\
     --filename %s \\
     --kind FetchedEntry \\
-    --url http://localhost:8080/admin/bulkload \\
-    --cookie <admin cookie for your app>""" % csv_filename
+    --url %s \\
+    --cookie <admin cookie for your app>""" % (csv_filename, bulkload_path)
 
 if __name__ == '__main__':
   main(sys.argv[1:])
